98f245cfe228fafec75bacafd8c7ba4add782cbd
Juan Rosario <jrosario19@hotmail.com>
2023-11-21 12:06:32 -0400

    Added migration

--- jfrs_personal_loans/Controllers/InAppPurchasesController.cs
+++ jfrs_personal_loans/Controllers/InAppPurchasesController.cs
@@ -77,6 +77,72 @@ namespace jfrs_personal_loans.Controllers
         }
 
         [HttpPost]
+        [Route("verifyorderexpiration")]
+        public IActionResult VerifyOrderExpiration([FromBody] InAppPurchase inAppPurchase)
+        {
+
+
+            
+
+            if (ModelState.IsValid)
+            {
+
+                var scopes = new[] { AndroidPublisherService.Scope.Androidpublisher };
+
+                ServiceAccountCredential credential;
+
+                using (var stream = new FileStream("key.json", FileMode.Open, FileAccess.Read))
+                {
+                    credential = GoogleCredential.FromStream(stream)
+                                                     .CreateScoped(scopes)
+                                                     .UnderlyingCredential as ServiceAccountCredential;
+
+
+                }
+
+                AndroidPublisherService androidPublisher = new AndroidPublisherService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Dash Clicker Juan Rosario",
+                });
+
+                MonetizationResource monetizationResource = new MonetizationResource(androidPublisher);
+
+                InappproductsResource inappproductsResource = new InappproductsResource(androidPublisher);
+
+                var listprod = inappproductsResource.List("jrs.personalloans.jrs_personal_loans").Execute();
+
+                var listsub = monetizationResource.Subscriptions.List("jrs.personalloans.jrs_personal_loans").Execute();
+
+                //var purchase = androidPublisher.Purchases.Subscriptions.Get("jrs.dashclicker", "dash_subscription_doubler", "fijdbilcdfjghmcibcmlfohg.AO-J1OwC_rdoNBGN-aJm873gNkIbLx0rQvEfaIeS47s0-GInEZjn5empw_4coPCyFFbl5BjKVXKYNJzlEyfqs80VjVHOxF4gYA").Execute();
+                var purchase = androidPublisher.Purchases.Subscriptions.Get("jrs.personalloans.jrs_personal_loans", "10_us_1_mes", "iahokldldhdkfngpmijglhfm.AO-J1Ox82olS39fOiYGWTkA0A40Q4k_s0aSn82-08kq-dH2AcUZy6m1Ozw-krD28GOzz3vkBgWYa8TaULFRP_vGifhn1cav-0AbY3WMqRJQ-29t285ilq5Q").Execute();
+                int i = 1 + 1;
+                //AndroidPublisherService androidPublisher=new AndroidPublisherService();
+                double ticks = double.Parse(purchase.ExpiryTimeMillis.ToString());
+                TimeSpan time = TimeSpan.FromMilliseconds(ticks);
+                DateTime expiredate = new DateTime(1970, 1, 1) + time;
+                //Console.WriteLine(expiredate.Ticks);
+                //Console.WriteLine(DateTime.Now.AddHours(4).Ticks);
+                //Console.WriteLine(DateTime.Now.AddHours(4).Ticks - expiredate.Ticks);
+                if (expiredate.Ticks < DateTime.Now.AddHours(4).Ticks)
+                {
+                    return Ok(new { message = "Order is passed due", expirationdate = expiredate.AddHours(-4) });
+                }
+                else {
+                    return Ok(new { message = "Order is current", expirationdate = expiredate.AddHours(-4) });
+                }
+                //Console.WriteLine(expiredate);
+
+                //Console.WriteLine(listsub.Subscriptions[0].ProductId);
+
+                //Console.ReadLine();
+            }
+
+            ModelState.AddModelError("message", "Invalid input attempt.");
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost]
         [Route("update")]
         public IActionResult Update([FromBody] InAppPurchase inAppPurchase)
         {
---
+++ jfrs_personal_loans/Migrations/20231116132753_ChangedDataTypeForInAppPurchaseModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jfrs_personal_loans.Migrations
+{
+    public partial class ChangedDataTypeForInAppPurchaseModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "InAppPurchaseToken",
+                table: "InAppPurchases",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "InAppPurchaseToken",
+                table: "InAppPurchases",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
